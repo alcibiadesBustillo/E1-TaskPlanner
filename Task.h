4cd770a5ba06ac5abb633cfb26de0edfa7e76c91
alcibiadesBustillo <59463430+alcibiadesBustillo@users.noreply.github.com>
2026-03-06 18:19:01 -0400

    Add Task class definition with member functions

---
+++ Task.h
@@ -0,0 +1,28 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include <string>
+#include <iostream>
+
+class Task {
+private:
+    std::string name;
+    int priority;
+    int duration;
+    int insertionOrder;
+
+public:
+    Task(const std::string& name = "", int priority = 1, int duration = 1, int insertionOrder = 0);
+
+    std::string getName() const;
+    int getPriority() const;
+    int getDuration() const;
+    int getInsertionOrder() const;
+
+    bool operator<(const Task& other) const;
+    bool operator==(const Task& other) const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Task& t);
+
+#endif
